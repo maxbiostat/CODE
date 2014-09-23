25b32ef1029b806841a94c8c965a32a8fcb6e223
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2014-09-23 19:16:42 -0300

    changes to the square normal distribution

--- R/DISTRIBUTIONS/square_normal.r
+++ R/DISTRIBUTIONS/square_normal.r
@@ -1,12 +1,13 @@
-dsqnorm<-function(x,mu=0,sd=1) {
- exp(-(x-2*abs(mu)*sqrt(x))/(2*sd^2))/(2*sqrt(2*pi*x)*sd*exp(mu^2/(2*sd^2)))
+dsqnorm <- function(x, mu = 0, sd = 1) {
+  exp(-(x-2*abs(mu)*sqrt(x))/(2*sd^2))/(2*sqrt(2*pi*x)*sd*exp(mu^2/(2*sd^2)))
 }
-Te <- 30
-mu1 <- 10
+Te <- 132
+mu1 <- 25
 mu2 <- Te-mu1
-sd<- 5
-X <- rnorm(10000,m=mu2,s=sd)
+sd <- 5
+X <- rnorm(10000, m = mu2, s = sd)
 hist(X)
-hist(X^2,prob=T)
-domain <- seq(range(X^2)[1],range(X^2)[2],.1)
-lines(domain,dsqnorm(x=domain,mu=mu2,sd=sd),lwd=3)
+hist(X^2, prob = TRUE, main = expression(Y == X^2), xlab = "Y")
+domain <- seq(range(X^2)[1], range(X^2)[2], .1)
+lines(domain, dsqnorm(x = domain, mu = mu2, sd = sd), lwd = 3)
+integrate(dsqnorm, mu = mu2, sd = sd, 0, Inf)
