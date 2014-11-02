6431bdde5f95eec95ed501aec0bca369a6c54301
Luiz Max Carvalho <lmax.procc@gmail.com>
2014-11-02 10:28:17 -0500

    Credit where it's due: the name is Gamma-ratio distribtution

--- R/DISTRIBUTIONS/gamma_ratio.R
+++ R/DISTRIBUTIONS/gamma_ratio.R
@@ -9,7 +9,6 @@
 # we can calculate the distribution and moments analytically.
 ## Copyleft(or the one to blame): Carvalho, LMF (2014)
 
-
 dgamma.ratio <- function(x, k1, k2, t1, t2, N){
   c <- {(N*t1*t2)^(k1+k2)}/(beta(k1,k2)*((N*t1)^k1)*(t2^k2)) # normalizing constant
   d <- x^(k1-1) * ((t2*x + N*t1))^(-(k1+k2)) # density 
@@ -17,9 +16,18 @@ dgamma.ratio <- function(x, k1, k2, t1, t2, N){
 }
 
 dgamma.ratio.Ncorrected<- function(x, k1, k2, t1, t2, N){
-  c <- {(N*t1*t2)^(k1+k2)}/(beta(k1,k2)*((N*t1)^k1)*(t2^k2)) 
-  S <- integrate(dgamma.ratio, k1 = k1,k2 = k2,t1 = t1, t2 = t2, N = N, 0, N)$value
+  c <- {(N*t1*t2)^(k1+k2)}/(beta(k1, k2)*((N*t1)^k1)*(t2^k2)) 
+  S <- integrate(dgamma.ratio, k1 = k1, k2 = k2, t1 = t1, t2 = t2, N = N, 0, N)$value
   cnew <- (1/S) * c # regularized normalizing constant
   d <- x^(k1-1) * ((t2*x + N*t1))^(-(k1+k2)) # density 
   return(cnew * d)
 }
+
+entropy.gamma.ratio <- function(k1, t1, k2, t2, N){
+  expectlog <- function(x){ -log(x) * dgamma.ratio(x, k1 = k1, t1 = t1,
+                                          k2 = k2, t2 = t2, N = N)}
+  return(integrate(expectlog, 0, Inf)$value)
+}
+# mean =  (k1/(k2-1))*(t1*N/t2)
+# mode = {(k1-1)*t1*N}/{t2*(k2+1)}
+# var = ((N*t1/t2)^2)* (k1*(k1+k2-1))/((k2-2)*(k2-1)^2)
