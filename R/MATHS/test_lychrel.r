8a14b473c4239eec3e3170637fa61b9a207a6013
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2015-08-29 00:59:41 +0100

    some remarks

--- R/MATHS/test_lychrel.r
+++ R/MATHS/test_lychrel.r
@@ -1,5 +1,7 @@
 ## Take a number and do 'revert and add' until you get a palindrome number
 ## Some code taken from http://stackoverflow.com/questions/19764244/how-can-we-split-an-integer-number-into-a-vector-of-its-constituent-digits-in-r
+## TODO: implement some efficient big number handling.
+## The objective is to run at least a million iterations on 196
 Lychrel <- function(x, itLimit = 1E+7){ ## play until find a palindrome
   splitDigits <- function(n) as.numeric(strsplit(as.character(n), "")[[1]]) ## get the digits
   collapseDigits <- function(v) as.numeric(paste(v, collapse = "")) ## get the number back
@@ -18,7 +20,7 @@ Lychrel <- function(x, itLimit = 1E+7){ ## play until find a palindrome
     res <- doIter(res$result)
     digis <- res$digits
     if(steps%%(itLimit/10)==0) cat("Done ", steps, "iterations already!",
-                                   "currently in", digis, "digits", "\n")
+                                   "currently in", digis, "digits \n")
     if(steps > itLimit) break()
   }
   return(list(result = res$result,
@@ -30,3 +32,4 @@ Lychrel(32)
 Lychrel(59)
 Lychrel(89)
 Lychrel(44)
+Lychrel(196) ## fails miserably see TODO
