31a337f65b4edac852429ece9f1aa27c87c1c0b9
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2015-08-29 02:07:28 +0100

    Now handles rather large numbers

--- R/MATHS/test_lychrel.r
+++ R/MATHS/test_lychrel.r
@@ -1,9 +1,13 @@
 ## Take a number and do 'revert and add' until you get a palindrome number
 ## Some code taken from http://stackoverflow.com/questions/19764244/how-can-we-split-an-integer-number-into-a-vector-of-its-constituent-digits-in-r
-## TODO: implement some efficient big number handling.
+## Uses Rmpfr for big number handling
 ## The objective is to run at least a million iterations on 196
 Lychrel <- function(x, itLimit = 1E+7){ ## play until find a palindrome
-  splitDigits <- function(n) as.numeric(strsplit(as.character(n), "")[[1]]) ## get the digits
+  require(Rmpfr)
+  splitDigits <- function(n) {## get the digits
+    stringz <- strsplit(Rmpfr::formatMpfr(mpfr(n, precBits = 1024 ), 1000), "\\.")[[1]][1]
+    return(as.numeric(strsplit(stringz, "")[[1]]))
+  }
   collapseDigits <- function(v) as.numeric(paste(v, collapse = "")) ## get the number back
   doIter <- function(input){ ## input is a number
     revinput <- rev(splitDigits(input))
@@ -19,8 +23,9 @@ Lychrel <- function(x, itLimit = 1E+7){ ## play until find a palindrome
     steps <- steps + 1
     res <- doIter(res$result)
     digis <- res$digits
-    if(steps%%(itLimit/10)==0) cat("Done ", steps, "iterations already!",
-                                   "currently in", digis, "digits \n")
+    if(digis%%50==0) cat("We are over", digis ,"digits now! \n")
+    if(steps%%(itLimit/.001*itLimit)==0) cat("Done ", steps, "iterations already!",
+                                             "currently in", digis, "digits \n")
     if(steps > itLimit) break()
   }
   return(list(result = res$result,
