617285ae3466a2f2b91ce21ea0139e0700b31d53
Luiz Max de Carvalho <lmcarvalho@ufrj.br>
2016-08-07 17:51:37 +0100

    minor spacing

--- R/GENERAL/integral_x^n_exp(ax).R
+++ R/GENERAL/integral_x^n_exp(ax).R
@@ -1,7 +1,7 @@
  f1 <- function(x, n, a) x^n * exp(a*x)
  f2 <- function(x, n, a) x^{n-1} * exp(a*x) 
 
- sol1 <- function(x, n, a)  (x^n * exp(a*x))/a - (n/a) * integrate(f2,n=n,a=a, 0, Inf)$value
+ sol1 <- function(x, n, a)  (x^n * exp(a*x))/a - (n/a) * integrate(f2, n = n, a = a, 0, Inf)$value
  # analytic solution given by Gradshteyn & Ryzhik
  # works for every a<0
  sol2 <- function(n, a) gamma(n+1) * (1/a)^{n+1}
@@ -11,7 +11,7 @@
  n <- 3
  x <- seq(.1,10,.1)
  
- plot(x, f1(x, n, a), type="l", lwd=3)
- sol1(x=10, n=n, a=a)
+ plot(x, f1(x, n, a), type = "l", lwd = 3)
+ sol1(x = 10, n = n, a = a)
  sol2 (n, a)
  
